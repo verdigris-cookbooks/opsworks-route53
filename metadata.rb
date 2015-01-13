223eb4438b6a646747570b9e55ae48555ce53a93
Andrew Jo <andrewjo@gmail.com>
2015-01-13 12:28:41 -0800

    Require route53 cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'opsworks_route53'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
-license          'all_rights'
-description      'Installs/Configures opsworks_route53'
-long_description 'Installs/Configures opsworks_route53'
+maintainer       'Andrew Jo'
+maintainer_email 'andrew@verdigris.co'
+license          'Simplified BSD'
+description      'Sets up AWS Route53 DNS table with instance hostname and FQDN'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'route53'

7d05d259d318a5d8e1e9d428a344a0d4d3ce5446
Andrew Jo <andrewjo@gmail.com>
2015-01-13 12:31:08 -0800

    Add default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,48 @@
 # Cookbook Name:: opsworks_route53
 # Recipe:: default
 #
-# Copyright (c) 2015 The Authors, All Rights Reserved.
+# Copyright (c) 2015, Verdigris Technologies Inc
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+include_recipe "route53"
+
+z_id = node[:opsworks][:route53][:zone_id]
+domain = node[:opsworks][:route53][:domainname]
+
+unless z_id
+  Chef::Application.fatal! "Cannot set zone record sets without zone ID."
+end
+
+unless domain
+  Chef::Application.fatal! "Domain attribute must be set."
+end
+
+route53_record "create a record" do
+  name      "#{node[:opsworks][:instance][:hostname]}.#{domain}"
+  value     node[:opsworks][:instance][:private_ip]
+  type      "A"
+  zone_id   z_id
+  overwrite true
+  action    :create
+end

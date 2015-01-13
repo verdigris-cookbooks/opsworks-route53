f767616c81ed088b77967abd40dba09474e4ece2
Andrew Jo <andrewjo@gmail.com>
2015-01-13 14:17:05 -0800

    Bump to v0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andrew@verdigris.co'
 license          'Simplified BSD'
 description      'Sets up AWS Route53 DNS table with instance hostname and FQDN'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w(amazon debian ubuntu).each do |platform|
   supports platform
